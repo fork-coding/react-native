f9e80b2c7f03de198f7ab6a814c7caff2ef71065
yiyi17 <lileibao13@163.com>
2024-05-12 16:04:16 +0800

    feat: init

---
+++ Test/TurboModuleTest.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// react-native/src/private/specs/modules
+const TurboModuleTest = () => {
+  // 拿到 React Native 的所有 TurboModule
+  const TurboModule = require('react-native');
+  
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default TurboModuleTest;
